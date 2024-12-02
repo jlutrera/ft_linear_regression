c4a6ec091e46f2096c7fbdc5f9d3c1bd12855955
jutrera- <jl_utrera_canalejo@yahoo.es>
2024-12-02 21:25:17 +0100

    cambios en __init__.py para evitar cosas raras

--- programs/__init__.py
+++ programs/__init__.py
@@ -21,3 +21,5 @@ K      = 0.0001 # Decay rate
 ALPHA_0= 0.1   # Initial learning rate
 EPSILON= 1e-8 # Stop when the loss is not decreasing significantly
 ITER   = 1000000 # Maximum number of iterations
+
+__all__ = ["RESET", "B_ON_W", "RED", "GREEN", "YELLOW", "BLUE", "MAGENTA", "CYAN", "THETA", "ALPHA", "SQUARE", "SUB0", "SUB1", "DOT", "K", "ALPHA_0", "EPSILON", "ITER"]
